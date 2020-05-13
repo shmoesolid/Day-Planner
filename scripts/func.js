dcb9ee8c8ba570017e8385b5cb4997ddaf23b1ae
Shane Koehler <shane.koehler@gmail.com>
2020-05-13 05:19:36 -0500

    adding more functionality..

--- scripts/func.js
+++ scripts/func.js
@@ -1,5 +1,38 @@
-// setup functions for saving and getting local timeslot
 
+/////////////////////////////////////////////////////////////////////////
+/** loads obj from local storage from unique name (default obj if null)
+ * 
+ * @param {string} storageName 
+ * @param {obj} defaultObj 
+ */
+function loadStorageVars(storageName, defaultObj)
+{
+    // attempt to load vars
+    var vars = JSON.parse(localStorage.getItem(storageName));
+
+    // no vars, return our default
+    if ( vars === null ) return defaultObj;
+
+    // we have something, return vars
+    return vars;
+}
+
+/////////////////////////////////////////////////////////////////////////
+/** saves obj into local storage by unique name
+ * 
+ * @param {string} storageName 
+ * @param {obj} obj 
+ */
+function saveStorageVars(storageName, obj)
+{
+    localStorage.setItem(storageName, JSON.stringify(obj));
+}
+
+/////////////////////////////////////////////////////////////////////////
+/** converts 24 hour time number only string to 12 hour am/pm
+ * 
+ * @param {string} number 
+ */
 function _convertTo12Hour(number)
 {
     // setup return array
@@ -8,7 +41,7 @@ function _convertTo12Hour(number)
     // error if not a number
     if (isNaN(array[0])) return false;
 
-    // convert to standard, but don't set if we are at 24 (below handles this)
+    // convert to standard with period change
     if (array[0] > 11 && array[0] < 24) 
     {
         array[0] -= 12;
@@ -16,9 +49,82 @@ function _convertTo12Hour(number)
     }
 
     // correct 0 and 24 results
-    if (array[0] == 0 || array[0] == 24) 
-        array[0] = 12;
+    if (array[0] == 0 || array[0] == 24) array[0] = 12;
 
     // return array
     return array;
+}
+
+/////////////////////////////////////////////////////////////////////////
+/** creates time slot row from index
+ * 
+ * @param {int} index 
+ * @param {bool} insertBefore 
+ */
+function createTimeslot(index, insertBefore = false)
+{
+    // get converted time data from index
+    var timeData = _convertTo12Hour(index);
+
+    // create section element
+    var slot = $("<section>");
+
+    // setup whole slot
+    slot.addClass("row");
+    slot.attr("id", index);
+    
+    // setup hour column
+    var hourCol = $("<div>");
+    hourCol.addClass("hour");
+    hourCol.text(timeData[0] + timeData[1]);
+    slot.append(hourCol);
+
+    // setup text area column
+    var areaCol = $("<textarea>")
+    slot.append(areaCol);
+
+    // setup save button
+    var saveCol = $("<button>");
+    saveCol.addClass("saveBtn far fa-save");
+    slot.append(saveCol);
+
+    // append and prepend our slot to time container
+    if (insertBefore) containerElm.prepend(slot);
+    else containerElm.append(slot);
+
+    // return our textarea col
+    // might return whole slot later, not needed currently
+    return areaCol;
+}
+
+/////////////////////////////////////////////////////////////////////////
+/** removes time slot 'row' by index
+ * 
+ * @param {int} index 
+ */
+function removeTimeslot(index)
+{
+    // simple enough
+    $(index).remove();
+}
+
+/////////////////////////////////////////////////////////////////////////
+/** does initial load of all the timeslots per the timeframe
+ * 
+ */
+function initTimeslots()
+{
+    // go through all our time slots
+    for (let i = 0; i < 25; i++) 
+    {
+        // check if within our time
+        if (i >= storage.timeStart && i <= storage.timeEnd)
+        {
+            // create the slot
+            var textarea = createTimeslot(i);
+
+            // add text if we have anything
+            textarea.text( (storage.timeSlots[i]) ? storage.timeSlots[i] : "" );
+        }
+    }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,50 @@
 
-// setup storage object for saving/loading data
+// setup unique local save/load namespace
+const SAVE_NAME = "SK_DAY_PLANNER";
+
+// get current date/time
+var now = moment();
+
+// setup default storage object for saving/loading data
 var storage = 
 {
     timeSlots: new Array(25), // 25 indexes for 0 to 24 indicating our hours in military
-    timeStart: 9,
-    timeEnd: 17
+    timeStart: 9, // 9am
+    timeEnd: 17, // 5pm
+    date: now // this date is for comparison of storage to now
 };
+
+// attempt to load from storage
+storage = loadStorageVars(SAVE_NAME, storage);
 
 // element references
 var currentDayElm = $('#currentDay');
-var containerElm = $('#container');
+var containerElm = $('#timeContainer');
 var timeStartElm = $('#timeStart');
 var timeEndElm = $('#timeEnd');
 var timeSliderElm = $('#slider-range');
 
-// planner time range slider/input setup
+// planner time range (jquery ui) slider/input setup
 timeSliderElm.slider
 ({
     // set properties
     range: true,
-    min: 0,
-    max: 24,
+    min: 0, max: 24,
     values: [ storage.timeStart, storage.timeEnd ],
 
     // setup event for setting inputs when slider changes
     slide: function( event, ui )
     {
+        // get time conversion data
         var startValue = _convertTo12Hour(ui.values[ 0 ]);
         var endValue = _convertTo12Hour(ui.values[ 1 ]);
 
+        // set display input values utilizing data
         timeStartElm.val(startValue[0] + startValue[1]);
         timeEndElm.val(endValue[0] + endValue[1]);
 
-        // adjust timeslots
+        // adjust timeslots here
+        // TODO..
     }
 });
 
@@ -42,27 +54,24 @@ var endValue = _convertTo12Hour(timeSliderElm.slider("values", 1 ));
 timeStartElm.val( startValue[0] + startValue[1] );
 timeEndElm.val( endValue[0] + endValue[1] );
 
-// get current date/time
-var now = moment();
-
-// format current day and display
+// format current day and display for header
 currentDayElm.text(now.format("MMMM Do YYYY"));
 
 //console.log(now.format("H")); // returns current time in 24 hour format without leading 0
 //console.log(_convertTo12Hour(now.format("H")));
 
-// setup event for changing time within start/end time inputs to have slider match
-// TODO..
+initTimeslots();
 
-// loads up schedule
-function loadSchedule()
+// setup listener for save button(s)
+$(".saveBtn").on("click", function()
 {
-    for (let i = 0; i < 25; i++) 
-    {
-        
+    // get parent aka time slot
+    var parent = $(this).parent();
 
-        
-    }
-}
+    // get time slot index from parent id of button
+    var timeIndex = parent.attr('id');
 
-
+    // ..
+    // parent
+    // storage.timeSlots[ timeIndex ] = 
+});
